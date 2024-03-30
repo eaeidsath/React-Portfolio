34b412fc6264fdc9ebb0622018c301e8cf30e55c
eaeidsath <evelyneidsath@gmail.com>
2024-03-30 16:35:26 -0700

    create header and footer files, fill out main and app jsx files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+import ProjectList from './components/ProjectList';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+function App() {
+    return (
+        <div className='projects'>
+            <ProjectList />
+        </div>
+    );
+}
+
+export default App;
