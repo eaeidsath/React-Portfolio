f44f64bdca25108aea69aeb9de043b4b6b6d9dad
EE <evelyneidsath@gmail.com>
2024-03-30 20:49:26 -0700

    establish usestate in portfolio container for page changes

---
+++ src/components/PortfolioContainer.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import Header from './Header';
+import About from './pages/About';
+import ProjectList from './pages/ProjectList';
+import Contact from './pages/Contact';
+import Resume from './pages/Resume';
+import { render } from 'react-dom';
+
+export default function PortfolioContainer() {
+    const [currentPage, setCurrentPage] = useState('About');
+
+    const renderPage = () => {
+        if (currentPage === 'About') {
+            return <About />
+        }
+        if (currentPage === 'ProjectList') {
+            return <ProjectList />
+        }
+        if (currentPage === 'Contact') {
+            return <Contact />
+        }
+        if (currentPage === 'Resume') {
+            return <Resume />
+        }
+    }
+
+    const handlePageChange = (page) => setCurrentPage(page);
+
+    return (
+        <div>
+            <Header currentPage={currentPage} handlePageChange={handlePageChange} />
+            <main className=""> {renderPage()}</main>
+        </div>
+    );
+}
