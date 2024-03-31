ac3a8e881c0894ed861f842b5ab75d452b92dea2
EE <evelyneidsath@gmail.com>
2024-03-30 20:35:11 -0700

    add separate directory for pages, create new page files, add header and footer code

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="">
+            <div className="">
+                <h4>
+                    Made by Evelyn Eidsath.
+                </h4>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+    return (
+        <header className="">
+            <div className="">
+                <h1 className="">Evelyn Eidsath</h1>
+                <p className="">Full-stack web developer</p>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
