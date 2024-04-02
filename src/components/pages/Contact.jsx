f3ae89b80a8d27739efd1a55eac6bebb2aab32ed
EE <evelyneidsath@gmail.com>
2024-04-01 17:17:38 -0700

    fill out contacts page

---
+++ src/components/pages/Contact.jsx
@@ -0,0 +1,27 @@
+export default function Contact() {
+    return (
+        <div class="contacts">
+            <h4>
+                Contact me
+            </h4>
+            <p>
+                I'm open to job inquiries, programming questions, or even just making a connection!
+            </p>
+            <section>
+                <a href="mailto: evelyneidsath@gmail.com">evelyneidsath@gmail.com</a>
+            </section>
+
+            <section>
+                <a href="https://www.linkedin.com/in/evelyn-eidsath-915b9b226/" target="_blank">Linkedin</a>
+            </section>
+            
+            <section>
+                <a>(123) 456-7890</a>
+            </section>
+
+            <section>
+                <a href="https://github.com/eaeidsath" target="_blank">GitHub</a>
+            </section>
+        </div>
+    )
+}
