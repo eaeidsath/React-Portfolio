199baceceeb7d9ee356cc3cb29cfd0c580f0975a
EE <evelyneidsath@gmail.com>
2024-04-01 21:24:07 -0700

    add bootstrap package and import, edit about me info

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,10 +1,18 @@
 export default function About() {
     return (
         <div>
-            <h1>About Page</h1>
-            <p>Hello and welcome to my portfolio. This website as well as all the projects linked here are examples of my web design experience. I starting learning to code in HTML and CSS last year, and I am currently taking a course to become certified in full-stack web development. The languages I am currently studying include HTML, CSS, and JavaScript. I will continue updating this portfolio as I gain more experience, learn new coding languages, and create new projects.</p>
-            <p>Here's a little background about myself. I graduated from UC Davis in 2020 with a Bachelor of Science in Food Science. From 2018 to 2021, I worked as a lab technician in a research lab owned by the USDA Agricultural Research Service (ARS), where I maintained insect colonies for pesticide testing. After that, I worked as a Quality Assurance Technician for Kerry Taste & Nutrition, and later in the same role for a Safeway-owned dairy processing plant. In those roles I performed many analytical tests, prepared microbiological incubations, and processed large amounts of data in laboratory information management system (LIMS) softwares. While these positions may not be directly related to coding and web development, they gave me invaluable experience that greatly improved my skills in decision-making, time management, communication, and teamwork, which are all vital to any successful career.</p>
-            <p>Please take a look at my projects by clicking the Projects List tab, and read me resume from the Resume tab. Reach out to me using the sources listed on the contacts page.
+            <h1>About Me</h1>
+            <p>
+                Hello and welcome to my portfolio. This website as well as all the projects linked here are examples of my web design experience. I starting learning front-end coding in 2022, and in 2023 I began a course to become certified in full-stack web development.I have gained in Javascript and Javascript related software such as nodeJS, Express, and React. I also have experience using databases including SQL and NoSQL databases such as MongoDB. I will continue updating this portfolio as I gain more experience, learn new coding languages, and create new projects.
+            </p>
+            <p>
+                Here's a little background about myself. I graduated from UC Davis in 2020 with a Bachelor of Science in Food Science. From 2018 to 2021, I worked as a lab technician in a research lab owned by the USDA Agricultural Research Service (ARS), where I maintained insect colonies for pesticide testing. After that, I worked as a Quality Assurance Technician for Kerry Taste & Nutrition, and later in the same role for a Safeway-owned dairy processing plant. In those roles I performed many analytical tests, prepared microbiological incubations, and processed large amounts of data in laboratory information management system (LIMS) softwares. While these positions may not be directly related to coding and web development, they gave me invaluable experience that greatly improved my skills in decision-making, time management, communication, and teamwork, which are all vital to any successful career.
+            </p>
+            <p>
+                I have been online since a young age and therefore have gained lifelong familiarity with computer software and developed an unending love for online culture. The innovation and potential surrounding the tech world is second to none, and I hope to contribute my skills and successes for the benefit of any and all who ever looked at a website and thought, "I want to make that, but better."
+            </p>
+            <p>
+                Please take a look at my projects by clicking the Projects List tab, and read me resume from the Resume tab. Reach out to me using the sources listed on the contacts page.
             </p>
         </div>
     );
